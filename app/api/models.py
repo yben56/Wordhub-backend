2fe717fde505cc6b1c99ea12a8088cc67b142159
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-06-02 02:07:53 +0800

    add api model

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Dictionary(models.Model):
+    id = models.AutoField(primary_key=True)
+    word = models.CharField(max_length=255, null=False)
+    phonetic = models.CharField(max_length=255, null=True)
+    pos = models.CharField(max_length=30, null=True)
+    translation = models.CharField(max_length=255, null=False)
+    sentences = models.TextField(null=True)
+    associate = models.TextField(null=True)
+    
+class Quiz(models.Model):
+    wordid = models.ForeignKey(Dictionary, on_delete=models.CASCADE, db_column='wordid')
+    word = models.CharField(max_length=255, null=False)
+    quiz = models.TextField(null=False)
+
+class Frequency(models.Model):
+    word = models.CharField(max_length=255, null=False)
+    frequency = models.IntegerField()
