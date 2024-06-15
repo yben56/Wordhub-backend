d61831326c065fcdb0736d87c6ecf34a5113618a
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-06-15 23:38:24 +0800

    update history

--- app/api/serializers/history_serializers.py
+++ app/api/serializers/history_serializers.py
@@ -4,12 +4,14 @@ from api.models import Dictionary, SearchWord
 class DictionarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Dictionary
-        fields = ['id', 'word']
+        fields = ['id', 'word', 'pos', 'translation']
 
 class HistorySerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(source='dictionary.id')
     word = serializers.CharField(source='dictionary.word')
+    pos = serializers.CharField(source='dictionary.pos')
+    translation = serializers.CharField(source='dictionary.translation')
     
     class Meta:
         model = SearchWord
-        fields = ['id', 'word', 'count', 'date']
+        fields = ['id', 'word', 'pos', 'translation', 'count', 'date']
