0ea1c2654937046795b95e5be61de7b84c01339d
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-06-19 22:28:00 +0800

    add chatgpt into utils

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,5 +1,7 @@
 from .models import Answer
 from api.serializers.answer_serializers import AnswerSerializer
+from openai import OpenAI
+import os
 
 def calculate_accuracy(user_id, word):
     fetchanswer = Answer.objects.filter(user_id=user_id, word=word).first()
@@ -12,3 +14,20 @@ def calculate_accuracy(user_id, word):
     )
 
     return output
+
+def chat_gpt(command):
+    model = os.environ.get('CHAT_GPT_MODEL', 'CHAT_GPT_MODEL not found')
+    client = OpenAI(api_key=os.environ.get('CHAT_GPT_API_KEY', 'CHAT_GPT_API_KEY not found'))
+
+    return command
+
+    '''
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "user", "content": command},
+        ]
+    )
+    
+    return response.choices[0].message.content
+    '''
