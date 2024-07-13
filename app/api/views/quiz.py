a7c2fa96498c0515dc08e13ae8c77b2cdbdc437c
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-07-14 01:41:12 +0800

    update quiz with recommand algorith

--- app/api/views/quiz.py
+++ app/api/views/quiz.py
@@ -3,56 +3,76 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-from ..models import Dictionary, Quiz
-from api.serializers.dictionary_serializers import DictionarySerializer
+from ..models import Quiz
 from api.serializers.quiz_serializers import QuizSerializer
+from ..utils import calculate_accuracy
 
-import json, os, random
+import pandas as pd
+import json, ast
 
 @api_view(['GET'])
 def quiz(request):
     #1. items
-    items = request.GET.get('items')
-
-    if items is not None:
-        try:
-            items = int(items)
-        except ValueError:
-            return Response({
-                'error' : True,
-                'message' : 'Invalid items'
-            }, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        items = 1 
+    try:
+        items = int(request.GET.get('items', 3))
+    except ValueError:
+        return Response({'error': True, 'message' : 'items be an integer'}, status=400)
     
-    #2. select quiz
-    word = Quiz.objects.filter(deleted=False).order_by('?')[:items]
-    serializer = QuizSerializer(word, rand=True, many=True)
-    data = serializer.data
-
-    #3. user mode (evaluation)
     if request.user_id:
-        from ..utils import calculate_accuracy
+        #3. get recommand list
+        recommand = request.GET.get('recommand', [])
 
+        if len(recommand) != 0:
+            try:
+                recommand = ast.literal_eval(recommand)
+            except (SyntaxError, ValueError) as e:
+                return Response({'error': True, 'message' : 'recommand must be a list'}, status=400)
+        
+        #4. prevent recommand redundant
+        if len(recommand) > items:
+            return Response({'error': True, 'message' : 'items mus >= recommand length'}, status=400)
+
+        #5. select recommand words
+        if len(recommand) > 0:
+            recommandquiz = Quiz.objects.filter(word__in=list(recommand), deleted=False).order_by('?')
+            serializer = QuizSerializer(recommandquiz, rand=True, many=True)
+            recommandquiz = serializer.data
+
+            #transform to df & select unique rand
+            recommandquiz = pd.DataFrame(recommandquiz)
+            recommandquiz = recommandquiz.groupby("word").sample(n=1, random_state=1).reset_index(drop=True)
+        else:
+            recommandquiz = pd.DataFrame()
+
+        #6. select rand words
+        if items - len(recommand) > 0:
+            randquiz = Quiz.objects.filter(deleted=False).order_by('?')[:items - len(recommand)]
+
+            serializer = QuizSerializer(randquiz, many=True)
+            randquiz = serializer.data
+
+            #transform to df
+            randquiz = pd.DataFrame(randquiz)
+        else:
+            randquiz = pd.DataFrame()
+
+        #7. merge recommandwords & randwords & transform to json
+        data = pd.concat([recommandquiz, randquiz], ignore_index=True)
+        data = data.to_json(orient='records', force_ascii=False)
+        data = json.loads(data)
+
+        #8. user mode (evaluation)
         for index in range(len(data)):
             data[index]['evaluation'] = calculate_accuracy(request.user_id, data[index]['word'])
-    
+    else:
+        #9. guess mode
+        quiz = Quiz.objects.filter(deleted=False).order_by('?')[:items]
+        serializer = QuizSerializer(quiz, rand=True, many=True)
+        data = serializer.data
 
+    
     return Response({
         'error' : False,
         'message' : '',
         'data' : data
-    }, status=200)
-        
-    #select random 10 quiz according to search & query
-    
-
-    #2. resposne
-    return Response({
-        'error' : False,
-        'message' : '',
-        'data' : response
-    }, status=200)
-
-def shuffle(row):
-    return json.loads(row)
+    }, status.HTTP_200_OK)
