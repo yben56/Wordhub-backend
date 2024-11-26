04232a046f221e64aaad35bb5a2757a950a60e5e
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2024-11-26 21:39:30 +0800

    update setting, 1. turn csrf on, 2. set cors to env

--- app/app/settings.py
+++ app/app/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -63,7 +63,9 @@ MIDDLEWARE = [
     
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('SITE_URL', 'LOCALHOST_3000 not found')
+]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_HEADERS = [
